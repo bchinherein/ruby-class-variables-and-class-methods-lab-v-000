1f46e6da036ce44b942cb3c8e13bdca1be549bb6
Brandon Chin <brandonchin@me.com>
2018-03-08 04:45:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,11 +43,11 @@ class Song
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count.has_key?(artist) == true
+      if artist_count.has_key?(artist)
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
-      end
+      end #if artist_count.has_key?
     end #@@artists.each
   end #def self.artist_count
 
